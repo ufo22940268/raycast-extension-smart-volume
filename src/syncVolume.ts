f2cc712fe1678991455cd5bdac5df7122877f997
Chao Cheng <ufo22940268@gmail.com>
2023-05-31 21:28:25 +0800

    only shows external speaker info in lcd.

--- src/syncVolume.ts
+++ src/syncVolume.ts
@@ -1,13 +1,15 @@
 import { m1ddc, updateVolume } from "./utils";
 
 async function getVolume() {
+  const vol = await m1ddc(["get", "volume"]);
   const muted = await m1ddc(["get", "mute"]) == '1'
   if (muted) return 0;
-  return await m1ddc(["get", "volume"])
+  return vol
 }
 
 export default async () => {
   const volume = await getVolume();
   const vol = Number(volume);
+  console.log(vol);
   await updateVolume(vol);
 }
