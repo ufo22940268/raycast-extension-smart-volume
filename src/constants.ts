a8b0f3fb92f8100cad667fa721aeaf2916da8e61
Chao Cheng <ufo22940268@gmail.com>
2023-11-02 10:12:03 +0800

    make volume step configurable.

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
-export const ADJUST_STEP = 5;
+import { getPreferenceValues } from "@raycast/api";
+
+export const ADJUST_STEP = (() => {
+  return Number.parseInt(getPreferenceValues()["volume_step"]);
+})();
 export const ABNORMAL_VOLUME = -92;
